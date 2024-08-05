aee4bde78562171fa7a358e943eb971dbf166cbc
Abdallh ELzayat <121009680+AbdallhElzayat2020@users.noreply.github.com>
2024-08-05 17:44:16 +0300

    Admin Dashboard Routes & Controller

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,18 @@
 <?php
 
-use App\Http\Controllers\Admin\HomeController;
+use App\Http\Controllers\Frontend\HomeController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-// Route::get('/', function () {
-//     return view('Frontend.home');
-// });
+
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
 //DASHBOARD
 //Route::get('/dashboard', function () {
 //    return view('admin.index');
 //})->name('home');
 
 
+//import dashboard Route (admin Route)
 require __DIR__ . '/dashboard.php';
