9d4f917992fe901553b9f29264f0fa8cee08db02
Abdallh ELzayat <121009680+AbdallhElzayat2020@users.noreply.github.com>
2024-08-06 05:58:05 +0300

    components

---
+++ config/admin_sidebar.php
@@ -0,0 +1,10 @@
+<?php
+return [
+
+    'menu' => [
+        'Dashboard' => [
+            'icon' => 'fas fa-fire',
+            'url' => 'dashboard',
+        ],
+    ],
+];
--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -1,21 +1,12 @@
 <div class="main-sidebar sidebar-style-2">
     <aside id="sidebar-wrapper">
         <div class="sidebar-brand">
-            <a href="index.html">Stisla</a>
-        </div>
-        <div class="sidebar-brand sidebar-brand-sm">
-            <a href="index.html">St</a>
+            <a href="#">JAVA</a>
         </div>
         <ul class="sidebar-menu">
-            <li class="menu-header">Dashboard</li>
-            <li class="dropdown active">
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
-                <ul class="dropdown-menu">
-                    <li class=active><a class="nav-link" href="#">General Dashboard</a></li>
-                    <li><a class="nav-link" href="#">Ecommerce Dashboard</a></li>
-                </ul>
-            </li>
+                <li class="dropdown active">
+                    <a class="nav-link" href="#">General Dashboard</a>
+                </li>
         </ul>
-
     </aside>
 </div>
